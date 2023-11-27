ccae97930b77872b898a54f8dfb31181c05aa8ef
MdShalimSadman <shalimsadmandipro3@gmail.com>
2023-11-27 23:34:15 +0600

    styles added

--- src/screen/customerCheckout.js
+++ src/screen/customerCheckout.js
@@ -1,21 +1,63 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const CustomerCheckout = ({ route }) => {
   const { farmerName, selectedProducts, totalPrice } = route.params;
 
-  // ... (rest of the code)
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 16,
+      backgroundColor: '#F5F5F5',
+    },
+    headerText: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: 'green',
+      marginBottom: 16,
+    },
+    productItem: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginBottom: 12,
+      backgroundColor: 'white',
+      padding: 10,
+      borderRadius: 5,
+    },
+    productName: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      color: 'black',
+    },
+    quantityText: {
+      fontSize: 16,
+      color: 'black',
+    },
+    totalPriceText: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: 'black',
+    },
+  });
 
   return (
-    <View>
-      {/* Display farmerName, selectedProducts, and totalPrice in your CustomerCheckout screen */}
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Order Summary</Text>
+
       <Text>Farmer: {farmerName}</Text>
-      {/* Loop through selectedProducts and display product name and quantity */}
-      {selectedProducts.map(product => (
-        <Text key={product.name}>{product.name}: {product.quantity}</Text>
-      ))}
-      <Text>Total Price: {totalPrice}</Text>
-      {/* ... (rest of the code) */}
+      <FlatList
+        data={selectedProducts}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => (
+          <View style={styles.productItem}>
+            <Text style={styles.productName}>{item.name}</Text>
+            <Text style={styles.quantityText}>Quantity: {item.quantity}</Text>
+          </View>
+        )}
+      />
+      <Text style={styles.totalPriceText}>Total Price: Taka {totalPrice}</Text>
+      {/* Additional components or features can be added here */}
     </View>
   );
 };
