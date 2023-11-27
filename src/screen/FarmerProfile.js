58fc5f150fa5fcddb121697a2a20c17453311c94
MdShalimSadman <shalimsadmandipro3@gmail.com>
2023-11-27 22:41:47 +0600

    the farmer profile full functional

--- src/screen/FarmerProfile.js
+++ src/screen/FarmerProfile.js
@@ -1,25 +1,59 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList, TextInput, TouchableOpacity } from 'react-native';
 
 const FarmerProfile = ({ route }) => {
-  const {name, products} = route.params;
+  const { name, products } = route.params;
+
+  const [cart, setCart] = useState({}); // State to track selected quantities
+
+  const handleQuantityChange = (product, quantity) => {
+    setCart(prevCart => ({
+      ...prevCart,
+      [product]: quantity,
+    }));
+  };
+
+  const handleAddToCart = () => {
+    // Implement your logic for handling the "Add to Cart" action
+    console.log('Cart:', cart);
+    // You can further implement the logic to send the cart data to a server or perform other actions.
+  };
+
+  const renderProductItem = ({ item }) => (
+    <View style={styles.productItem}>
+      <Text style={styles.productName}>{item}</Text>
+      <TextInput
+        style={styles.quantityInput}
+        placeholder="qty (kg)"
+        keyboardType="numeric"
+        placeholderTextColor="grey"
+        value={cart[item] ? cart[item].toString() : ''}
+        onChangeText={(text) => handleQuantityChange(item, text)}
+      />
+    </View>
+  );
 
   return (
     <View style={styles.container}>
-      <Text style={styles.headerText}>Farmer Profile</Text>
-      <Text style={styles.farmerName}>Name: {name}</Text>
-      <Text style={styles.productsList}>Products: {products.join(', ')}</Text>
+      <Text style={styles.headerText}>{name}'s Products</Text>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item}
+        renderItem={renderProductItem}
+      />
+      <TouchableOpacity onPress={handleAddToCart}>
+        <View style={styles.addToCartButton}>
+          <Text style={styles.addToCartButtonText}>Add to Cart</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginTop:15,
-    marginLeft:19,
-    justifyContent: 'start',
-    alignItems: 'start',
+    flex: 1,
+    padding: 16,
   },
   headerText: {
     fontSize: 24,
@@ -27,14 +61,36 @@ const styles = StyleSheet.create({
     color: 'green',
     marginBottom: 16,
   },
-  farmerName: {
+  productItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 12,
+  },
+  productName: {
     fontSize: 18,
     fontWeight: 'bold',
     color: 'black',
   },
-  productsList: {
-    fontSize: 16,
+  quantityInput: {
+    width: 80,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 5,
+    paddingLeft: 8,
     color: 'black',
+  },
+  addToCartButton: {
+    backgroundColor: 'green',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  addToCartButtonText: {
+    color: 'white',
+    fontSize: 18,
   },
 });
 
