0cb704f21b1d952800569369e6bec522fb8d5dbe
MdShalimSadman <shalimsadmandipro3@gmail.com>
2023-12-05 01:39:53 +0600

    timer added in bidding

--- src/screen/Bidding.js
+++ src/screen/Bidding.js
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 
-const Bidding = ({ route, navigation }) => {
-  const { estimatedPrice, totalPrice } = route.params;
+const Bidding = ({route, navigation}) => {
+  const {estimatedPrice, totalPrice} = route.params;
   const [userProposedPrice, setUserProposedPrice] = useState('');
+  const [timer, setTimer] = useState(60); // 5 minutes in seconds
+  const [timerExpired, setTimerExpired] = useState(false);
 
   const handleAccept = () => {
     // Implement logic for accepting estimated price
@@ -17,31 +25,64 @@ const Bidding = ({ route, navigation }) => {
     console.log('User Proposed Price:', userProposedPrice);
   };
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer(prevTimer => (prevTimer > 0 ? prevTimer - 1 : 0));
+    }, 1000);
+
+    // Clear interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (timer === 0) {
+      setTimerExpired(true);
+    }
+  }, [timer]);
+
+  const formatTime = seconds => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.headerText}>Bidding</Text>
-      <Text style={styles.estimatedPriceText}>Measured Price: Taka {totalPrice}</Text>
+      <Text style={styles.estimatedPriceText}>
+        Measured Price: Taka {totalPrice}
+      </Text>
       <Text style={styles.subHeaderText}>Choose an option:</Text>
 
       <TouchableOpacity style={styles.optionButton} onPress={handleAccept}>
         <Text style={styles.optionButtonText}>Accept and Proceed</Text>
       </TouchableOpacity>
+      {timerExpired ? (
+        <Text style={styles.timerExpiredText}>Time Limit Expired for bidding. Please tap on "Accept and Proceed" to proceed to payment</Text>
+      ) : (
+        <>
+          <Text style={styles.subHeaderText}>OR</Text>
 
-      <Text style={styles.subHeaderText}>OR</Text>
+          <Text style={styles.subHeaderText}>Propose a Price:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your proposed price"
+            keyboardType="numeric"
+            value={userProposedPrice}
+            onChangeText={text => setUserProposedPrice(text)}
+            placeholderTextColor="grey"
+            editable={!timerExpired}
+          />
 
-      <Text style={styles.subHeaderText}>Propose a Price:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter your proposed price"
-        keyboardType="numeric"
-        value={userProposedPrice}
-        onChangeText={text => setUserProposedPrice(text)}
-        placeholderTextColor="grey"
-      />
-
-      <TouchableOpacity style={styles.optionButton} onPress={handlePropose}>
-        <Text style={styles.optionButtonText}>Propose Price</Text>
-      </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.optionButton}
+            onPress={handlePropose}
+            disabled={timerExpired}>
+            <Text style={styles.optionButtonText}>Propose Price</Text>
+          </TouchableOpacity>
+        </>
+      )}
+        <Text style={styles.timerText}>Time Left: {formatTime(timer)}</Text>
     </View>
   );
 };
@@ -62,6 +103,17 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'black',
     marginBottom: 16,
+  },
+  timerText: {
+    fontSize: 18,
+    color: 'black',
+    marginTop: 8,
+  },
+  timerExpiredText: {
+    fontSize: 18,
+    color: 'red',
+    marginBottom: 16,
+    marginTop: 16,
   },
   subHeaderText: {
     fontSize: 18,
